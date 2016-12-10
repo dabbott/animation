af8acae1c537082e09fe4cd1d85c44b4493b08bb
Devin Abbott <devinabbott@gmail.com>
2016-12-10 08:49:02 -0800

    Build with webpack. Scroll driver works on web.

--- src/drivers/ScrollDriver.js
+++ src/drivers/ScrollDriver.js
@@ -17,15 +17,15 @@ export class ScrollDriver {
     this.value = new Animated.Value(0);
     this.onScrollViewLayout = this.onScrollViewLayout.bind(this);
     this.scrollViewProps = {
-      onScroll: Animated.event(
-        [{
-          nativeEvent: {
-            contentOffset: {
-              y: this.value,
-            },
-          },
-        }]
-      ),
+      // onScroll doesn't fire as expected, so use direct manipulation by
+      // listening to scroll directly on the DOM node.
+      ref: (scrollView) => {
+        const node = scrollView.getScrollableNode();
+
+        node.addEventListener('scroll', () => {
+          this.value.setValue(node.scrollTop)
+        });
+      },
       scrollEventThrottle: 1,
       onLayout: this.onScrollViewLayout,
     };
